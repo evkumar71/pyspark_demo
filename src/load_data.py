14da2143864ae839ac1d2466051ef2cda288926a
Vijaya K Eyunni <evkumar71@yahoo.com>
2024-02-10 12:41:27 -0800

    handling missing values

--- src/load_data.py
+++ src/load_data.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark.sql import Window
+import datetime
 
 file_schema = StructType([
     StructField('Date', DateType()),
@@ -22,9 +23,10 @@ spark = SparkSession \
 
 print(spark.version)
 
+
 class Readfile():
 
-    def load_file(self, symbol:str) -> DataFrame:
+    def load_file(self, symbol: str) -> DataFrame:
         fpath = f'datafiles/{symbol}.csv'
 
         # create a DataFrameReader
@@ -51,9 +53,9 @@ class Readfile():
         # df for SQL
         df3.createOrReplaceTempView("stock_info")
 
-        query = 'select date,'\
-                ' date_add(date, 10) as date_10,'\
-                f"concat(cast(date_10 as string), 'hello world' ) as new_col"\
+        query = 'select date,' \
+                ' date_add(date, 10) as date_10,' \
+                f"concat(cast(date_10 as string), 'hello world' ) as new_col" \
                 ' from stock_info'
 
         spark.sql('select * from stock_info').show(3, truncate=False)
@@ -61,10 +63,10 @@ class Readfile():
 
         # define window fn-ex1
         win = Window.partitionBy(year(df3['date']), month(df3['date'])) \
-                    .orderBy(df3['close'].desc(), df3['date'])
+            .orderBy(df3['close'].desc(), df3['date'])
 
         # apply window-fn on the win
-        df3.withColumn('rank', row_number().over(win))\
+        df3.withColumn('rank', row_number().over(win)) \
             .filter(col('rank') == 1) \
             .show(10)
 
@@ -73,18 +75,41 @@ class Readfile():
 
         df3.withColumn('prevDayClose', lag(df3['close'], 1, 0.0).over((win))) \
             .withColumn('diffPrevClose', df3['open'] - col('prevDayClose')) \
-            .withColumn('diffPrevCloseNew', when(col('prevDayClose') == 0.0, 0).otherwise(df3['open'] - col('prevDayClose'))) \
+            .withColumn('diffPrevCloseNew',
+                        when(col('prevDayClose') == 0.0, 0).otherwise(df3['open'] - col('prevDayClose'))) \
             .show()
 
         return df3
 
+    def df_test(self):
+        schema = StructType() \
+            .add('date', DateType(), True) \
+            .add('open', DoubleType(), True) \
+            .add('close', DoubleType(), True)
+
+        df = spark.createDataFrame([
+            Row(date=None, open=None, close=2.0),
+            Row(date=datetime.date(2024, 1, 31), open=1.0, close=2.0),
+            Row(date=datetime.date(2024, 1, 31), open=2.0, close=3.0),
+            Row(date=datetime.date(2024, 1, 31), open=3.0, close=None)
+        ], schema)
+
+        df.show()
+        default_dt = datetime.date(9999, 12, 31)
+        df.na.fill({'open': 0.0, 'close': 1.0})
+
+        df.withColumn('date', when(df['date'].isNull(), default_dt) \
+                      .otherwise((df['date']))).show()
+
 
 if __name__ == '__main__':
     cls = Readfile()
+    cls.df_test()
+
     df = cls.load_file('ZYNE')
     df.printSchema()
     df.sort(df['date'].asc()).show(5)
-    df.sort(df['date'].desc() , df['open'].asc()).show(5)
+    df.sort(df['date'].desc(), df['open'].asc()).show(5)
 
     df.groupby(year(df['date']).alias('Year'), month(df['date']).alias('Month')) \
         .agg(max('close').alias('maxClose'), avg('close').alias('avgClose'), sum('open').alias('sumOpen')) \
