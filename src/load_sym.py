fcf8292517dbe048b828a92d5d59cee28dcb6961
Vijaya K Eyunni <evkumar71@yahoo.com>
2024-02-17 23:51:29 -0800

    added agg fn

--- src/load_sym.py
+++ src/load_sym.py
@@ -29,14 +29,15 @@ sym_sch = StructType([
 
 symbols = ['ZUO', 'ZVO', 'ZYME', 'ZYNE', 'ZYXI']
 
+
 def datastore():
     global spark
 
     spark = SparkSession \
-            .builder \
-            .appName("spark etl") \
-            .master("local[*]") \
-            .getOrCreate()
+        .builder \
+        .appName("spark etl") \
+        .master("local[*]") \
+        .getOrCreate()
 
     # DataFrameReader
     csv_path = "datafiles/symbols_valid_meta.csv"
@@ -51,19 +52,33 @@ def datastore():
     for sym in symbols:
         find_max(sym)
 
+    for sym in symbols:
+        find_sma(sym)
+
     spark.stop()
+
 
 def find_max(sym: str):
     csv_path = f"datafiles/{sym}.csv"
-
     df = spark.read.csv(csv_path, schema=sym_sch, header=True)
+
     df2 = df.withColumn('sym', lit(sym))
     df2.groupby(df2['sym'], year(df2['Date']).alias('year')) \
         .agg(min('Close').alias('minClose'), max('Close').alias('maxClose')) \
         .orderBy(col('year')) \
         .show(5)
 
-    # df.select(min(df['Close']).alias(f"{sym} Min Close"), max(df['Close']).alias(f"{sym} Max Close")).show()
+
+# Simple Moving Average
+def find_sma(sym: str):
+    csv_path = f"datafiles/{sym}.csv"
+    df = spark.read.csv(csv_path, schema=sym_sch, header=True)
+
+    df.groupby(year(df['Date']).alias('year'), month(df['Date']).alias('month')) \
+        .agg(avg('Close').alias('simple moving avg')) \
+        .orderBy('year', 'month') \
+        .show(5)
+
 
 if __name__ == '__main__':
     datastore()
