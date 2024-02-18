6e64265e2ad56e6665c2f10d2517bca4278f79bb
Vijaya K Eyunni <evkumar71@yahoo.com>
2024-02-17 16:41:03 -0800

    added parq-file, agg-fun

--- src/load_sym.py
+++ src/load_sym.py
@@ -1,20 +1,33 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrameWriter, DataFrame
 from pyspark.sql.types import *
+from pyspark.sql.functions import min, max
 
-sch = StructType([
-    StructField("nasdaqTraded", StringType()),
-    StructField("symbol", StringType()),
-    StructField("securityName", StringType()),
-    StructField("listingExchange", StringType()),
-    StructField("marketCategory", StringType()),
-    StructField("etf", StringType()),
-    StructField("roundLotSize", DoubleType()),
-    StructField("testIssue", StringType()),
-    StructField("financialStatus", StringType()),
-    StructField("cqsSymbol", StringType()),
-    StructField("nasdaqSymbol", StringType()),
-    StructField("nextShares", StringType())
+meta_sch = StructType([
+    StructField("Nasdaq Traded", StringType()),
+    StructField("Symbol", StringType()),
+    StructField("Security Name", StringType()),
+    StructField("listing Exchange", StringType()),
+    StructField("Market Category", StringType()),
+    StructField("ETF", StringType()),
+    StructField("Round Lot Size", DoubleType()),
+    StructField("Test Issue", StringType()),
+    StructField("Financial Status", StringType()),
+    StructField("CQS Symbol", StringType()),
+    StructField("NASDAQ Symbol", StringType()),
+    StructField("NextShares", StringType())
 ])
+
+sym_sch = StructType([
+    StructField('Date', DateType()),
+    StructField('Open', DoubleType()),
+    StructField('High', DoubleType()),
+    StructField('Low', DoubleType()),
+    StructField('Close', DoubleType()),
+    StructField('Adj Close', DoubleType()),
+    StructField('Volume', LongType())
+])
+
+symbols = ['ZUO', 'ZVO', 'ZYME', 'ZYNE', 'ZYXI']
 
 def datastore():
     global spark
@@ -25,13 +38,26 @@ def datastore():
             .master("local[*]") \
             .getOrCreate()
 
-    # get DataFrameReader
-    fpath = "datafiles/symbols_valid_meta.csv"
-    df = spark.read.csv(fpath, schema=sch, header=True)
+    # DataFrameReader
+    csv_path = "datafiles/symbols_valid_meta.csv"
+    df = spark.read.csv(csv_path, schema=meta_sch, header=True)
+    df.show(10)
 
-    df.show()
+    # DataFrameWriter
+    pq_path = "derived/symbols.parquet"
+    df_writer = DataFrameWriter(df)
+    df_writer.parquet(path=pq_path, mode="overwrite")
+
+    for sym in symbols:
+        find_max(sym)
 
     spark.stop()
+
+def find_max(sym: str):
+    csv_path = f"datafiles/{sym}.csv"
+
+    df = spark.read.csv(csv_path, schema=sym_sch, header=True)
+    df.select(min(df['Close']).alias(f"{sym} Min Close"), max(df['Close']).alias(f"{sym} Max Close")).show()
 
 if __name__ == '__main__':
     datastore()
